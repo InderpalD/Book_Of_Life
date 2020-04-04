09c19d852b499e51f58dad9451cfaaa9036475a5
InderpalD <isdhillo@ucsd.edu>
2020-04-03 22:08:32 -0400

    Changed function based views to class based views

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 # Urls specific to the blog app.
 urlpatterns = [
     # The names of these paths are referenced in base.html
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
+
+
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
 
+''' We are going to use a list view since our home page
+is a list of blogs 
+'''
 
 def home(request):
     # Render the home.html file from blog/templates/blog
@@ -12,6 +16,16 @@ def home(request):
 
     # By passing additonal args into render, you can access in template file
     return render(request, 'blog/home.html', template_inputs)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     # Render the about.html file from blog/templates/blog
