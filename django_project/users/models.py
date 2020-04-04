b2eec92e0321860fea4b47ab82ce188ad235e1dc
InderpalD <isdhillo@ucsd.edu>
2020-04-03 21:27:02 -0400

    More profile functionality, forms

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     # Define relationship with the User for our database
@@ -8,3 +9,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # We are going to override the save function to scale images
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output = (300,300)
+            img.thumbnail(output)
+            img.save(self.image.path)
+
+
+
